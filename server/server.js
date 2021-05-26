1545b05c8b87cbe4a058838a6b55d87f78bfbf67
Chris Klemz <crklemz@gmail.com>
2021-05-26 06:05:26 -0500

    base mode complete

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const pg = require('pg'); // huge issue I ran into - npm install pg *******
-//pg configuration: LOCAL ONLY - try to put on huroku - wont work
+const pg = require('pg');
+
 const Pool = pg.Pool;
 const pool = new Pool({
     database: 'jazzy_sql', //name of database
@@ -29,56 +29,58 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-
-
-
-
-
 // TODO - Replace static content with a database tables
 
-
-
-const songList = [{
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
-
-app.get('/artist', (req, res) => { //didnt have a router set up so we changed from router.got to app.get
+app.get('/artist', (req, res) => {
 
     const queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
     pool.query(queryText)
         .then((result) => {
             console.log(result.rows);
             res.send(result.rows);
-        }).catch( (err) => {
+        }).catch((err) => {
             console.log(err);
             res.sendStatus(500);
         })
-
-
-
-});//end router.get
+});
 
 app.post('/artist', (req, res) => {
-    console.log('req.bod', req.body);
-    
+    console.log('req.body', req.body);
+
     let queryText = `INSERT INTO "artist"("name", "birthdate")
     VALUES ($1, $2);`
 
     let values = [req.body.name, req.body.birthdate];
-        pool.query(queryText, values)
+    pool.query(queryText, values)
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
+
+app.get('/song', (req, res) => {
+
+    const queryText = `SELECT * FROM "song" ORDER BY "title";`
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result.rows);
+            res.send(result.rows);
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
+
+app.post('/song', (req, res) => {
+    console.log('req.body', req.body);
+
+    let queryText = `INSERT INTO "song"("title", "length", "released")
+        VALUES ($1, $2, $3);`
+
+    let values = [req.body.title, req.body.length, req.body.released];
+    pool.query(queryText, values)
         .then((result) => {
             res.sendStatus(201);
         }).catch((err) => {
@@ -86,14 +88,4 @@ app.post('/artist', (req, res) => {
             res.sendStatus(500);
         })
 
-});
-
-app.get('/song', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songList);
-});
-
-app.post('/song', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
 });
